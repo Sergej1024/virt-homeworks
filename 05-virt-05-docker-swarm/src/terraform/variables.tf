6f4153010d84188bb4c7fa4a3f5230f367154e7a
Розум Сергей <sergej_r@list.ru>
2022-04-24 20:54:38 +0500

    update 5.5

--- 05-virt-05-docker-swarm/src/terraform/variables.tf
+++ 05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -1,17 +1,17 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
-  default = "b1gu1gt5nqi6lqgu3t7s"
+  default = "b1g3839rsq4g6pkhp2e7"
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
-  default = "b1gaec42k169jqpo02f7"
+  default = "b1gaa80uc55olksp2vtt"
 }
 
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
 variable "centos-7-base" {
-  default = "fd884f65mo7hpqd0suh4"
+  default = "fd8kni52af2sg80sgel1"
 }
